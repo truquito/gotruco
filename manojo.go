99ad5ae8e2f827eb5b274f5e347d754ef30f865f
Juampi Filevich <juan.filevich@fing.edu.uy>
2019-12-24 12:45:10 -0300

    nuevo metodo manojocalcFlor

--- manojo.go
+++ manojo.go
@@ -27,7 +27,7 @@ type Manojo struct {
 func (manojo Manojo) tieneFlor(muestra Carta) (res bool, CASO int) {
 	// CASO I: (al menos) dos piezas
 	numPiezas := 0
-	// en caso de que tenga al menos una pieza, 
+	// en caso de que tenga al menos una pieza,
 	// esta variable guarda uno su indice (usado en el caso III)
 	piezaIdx := -1
 	for i, carta := range manojo.Cartas {
@@ -49,8 +49,8 @@ func (manojo Manojo) tieneFlor(muestra Carta) (res bool, CASO int) {
 	// Y ESAS DOS DIFERENTES DE LA PIEZA (piezaIdx)!
 	tieneDosDelMismoPalo :=
 		(manojo.Cartas[0].Palo == manojo.Cartas[1].Palo && piezaIdx == 2) ||
-		(manojo.Cartas[0].Palo == manojo.Cartas[2].Palo && piezaIdx == 1) ||
-		(manojo.Cartas[1].Palo == manojo.Cartas[2].Palo && piezaIdx == 0)
+			(manojo.Cartas[0].Palo == manojo.Cartas[2].Palo && piezaIdx == 1) ||
+			(manojo.Cartas[1].Palo == manojo.Cartas[2].Palo && piezaIdx == 0)
 
 	if numPiezas == 1 && (tieneDosDelMismoPalo) {
 		return true, 3
@@ -60,13 +60,23 @@ func (manojo Manojo) tieneFlor(muestra Carta) (res bool, CASO int) {
 	return false, -1
 }
 
-func (manojo Manojo) cantarFlor(tipoFlor int, muestra Carta) {
-	var puntajeFlor int
+// retorna el valor de la flor de un manojo
+// si no tiene flor retorna 0 y error
+func (m *Manojo) calcFlor(muestra Carta) (int, error) {
+	var (
+		puntajeFlor         int
+		tieneFlor, tipoFlor = m.tieneFlor(muestra)
+	)
+
+	if !tieneFlor {
+		return -1, fmt.Errorf("Este manojo no tiene flor")
+	}
+
 	switch tipoFlor {
 	// CASO I: (al menos) dos piezas
 	case 1:
-		max := maxOf3(manojo.Cartas)
-		for _, carta := range manojo.Cartas {
+		max := maxOf3(m.Cartas)
+		for _, carta := range m.Cartas {
 			puntaje := carta.calcPuntaje(muestra)
 			if puntaje == max {
 				puntajeFlor += puntaje
@@ -77,11 +87,18 @@ func (manojo Manojo) cantarFlor(tipoFlor int, muestra Carta) {
 	// CASO II una pieza y dos cartas del mismo palo;
 	// CASO III: tres cartas del mismo palo,
 	case 2, 3:
-		for _, carta := range manojo.Cartas {
+		for _, carta := range m.Cartas {
 			puntajeFlor += carta.calcPuntaje(muestra)
 		}
 	}
-	fmt.Printf("%s CANTA FLOR: \"Tengo flor!\" (tiene %v)\n", "NOMBRE-JUGADOR", puntajeFlor)
+	return puntajeFlor, nil
+}
+
+// todo: esto se tiene que ir a la re mierda;
+// es una mezcla de capa de presentacion con logica; puaj
+func (manojo Manojo) cantarFlor(tipoFlor int, muestra Carta) {
+	x, _ := manojo.calcFlor(muestra)
+	fmt.Printf("%s CANTA FLOR: \"Tengo flor!\" (tiene %v)\n", "NOMBRE-JUGADOR", x)
 }
 
 // Print imprime la info del manojo
@@ -92,14 +109,14 @@ func (manojo Manojo) Print() {
 }
 
 // Print imprime la info del manojo
-func (manojo Manojo) getPerfil() *Jugador{
+func (manojo Manojo) getPerfil() *Jugador {
 	return manojo.jugador
 }
 
 // tiene2DelMismoPalo devuelve 'true' si tiene dos cartas
 // del mismo palo, y ademas los indices de las mismas en
 // el array manojo.Cartas
-func (manojo Manojo) tiene2DelMismoPalo() (bool, []int){
+func (manojo Manojo) tiene2DelMismoPalo() (bool, []int) {
 	for i := 0; i < CantCartasManojo; i++ {
 		for j := i + 1; j < CantCartasManojo; j++ {
 			mismoPalo := manojo.Cartas[i].Palo == manojo.Cartas[j].Palo
@@ -114,13 +131,13 @@ func (manojo Manojo) tiene2DelMismoPalo() (bool, []int){
 // calcularEnvido devuelve el puntaje correspondiente al envido del manojo
 // PRE: no tiene flor
 func (manojo Manojo) calcularEnvido(muestra Carta) (puntajeEnvido int) {
-	tiene2DelMismoPalo, idxs :=  manojo.tiene2DelMismoPalo()
+	tiene2DelMismoPalo, idxs := manojo.tiene2DelMismoPalo()
 	if tiene2DelMismoPalo {
-		x := manojo.Cartas[ idxs[0] ].calcPuntaje(muestra)
-		y := manojo.Cartas[ idxs[1] ].calcPuntaje(muestra)
+		x := manojo.Cartas[idxs[0]].calcPuntaje(muestra)
+		y := manojo.Cartas[idxs[1]].calcPuntaje(muestra)
 		noTieneNingunaPieza := max(x, y) < 27
 		if noTieneNingunaPieza {
-			puntajeEnvido = x + y + 20			
+			puntajeEnvido = x + y + 20
 		} else {
 			puntajeEnvido = x + y
 		}
