1267b4387f56a568db5e910cee00a269b2a9f7ed
Juampi Filevich <juan.filevich@fing.edu.uy>
2019-12-27 14:54:05 -0300

    eliminado metodo trivial

--- jugador.go
+++ jugador.go
@@ -12,16 +12,6 @@ type Jugador struct {
 	nombre string
 	// idx			JugadorIdx
 	equipo Equipo
-	manojo *Manojo
-}
-
-// NuevoJugador : Retorna un nuevo jugador,
-// con manojo vacio
-func NuevoJugador(nombre string) *Jugador {
-	return &Jugador{
-		nombre: nombre,
-		manojo: nil,
-	}
 }
 
 // Print imprime la info de `jugador`
