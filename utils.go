7925aa87a61ca65a396f8b6babd666997671eb13
Juampi Filevich <juanfilevich@gmail.com>
2020-05-23 02:24:48 -0300

    eliminado deprected utils.obtenerIdx

--- utils.go
+++ utils.go
@@ -71,16 +71,6 @@ func abs(x int) int {
 	return x
 }
 
-// obtenerIdx devuelve el ID correspondiente al argumento *Jugador
-func obtenerIdx(jugador *Jugador, jugadores []Jugador) (JugadorIdx, error) {
-	for i, j := range jugadores {
-		if j.Nombre == jugador.Nombre {
-			return JugadorIdx(i), nil
-		}
-	}
-	return -1, fmt.Errorf("Jugador `%s` no encontrado", jugador.Nombre)
-}
-
 // complemento de 'x in A, where A=[0, 1, 2]' devuelve
 // *por separado*, el conjunto ordenado 'A - x'
 // (A sin el elemento x)
