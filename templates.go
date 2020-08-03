4e6733c12ece94a51727a1276cfa5cb464a40115
Juampi Filevich <juan.filevich@fing.edu.uy>
2020-08-03 03:52:23 -0300

    templates parametrizados

--- templates.go
+++ templates.go
@@ -55,17 +55,26 @@ func (t templates) vacio() string {
 	return ""
 }
 
-func (t templates) carta(carta Carta) string {
-	valor, palo := carta.Valor, carta.Palo.String()
+func mkCarta(valor, palo string) string {
+
 	template := "┌xx┐" + "\n"
 	template += "│PP│" + "\n"
 	template += "└──┘"
 
-	numStr := t.renderValorCarta(valor)
-	template = canvas.Replace("xx", numStr, template)
-	template = canvas.Replace("PP", palo[:2], template)
+	template = canvas.Replace("xx", valor, template)
+	template = canvas.Replace("PP", palo, template)
 
 	return template
+}
+
+func (t templates) carta(carta Carta) string {
+	valor, palo := carta.Valor, carta.Palo.String()
+	numStr := t.renderValorCarta(valor)
+	return mkCarta(numStr, palo[:2])
+}
+
+func (t templates) cartaOculta(carta Carta) string {
+	return mkCarta("──", "  ")
 }
 
 func (t templates) cartaDobleSolapada(carta Carta) string {
@@ -98,47 +107,81 @@ func (t templates) cartaTripleSolapada(carta Carta) string {
 	return cartaTripleSolapada
 }
 
-func (t templates) cartaDobleVisible(cartas []Carta) string {
+func mkCartaDobleVisible(valores, palos []string) string {
 	cartaDobleSolapada := canvas.Raw(`
 ┌xx┐yy┐
 │PP│QQ│
 └──┘──┘
 `)
 
-	valor, palo := cartas[0].Valor, cartas[0].Palo.String()
-	numStr := t.renderValorCarta(valor)
-	cartaDobleSolapada = canvas.Replace("xx", numStr, cartaDobleSolapada)
-	cartaDobleSolapada = canvas.Replace("PP", palo[:2], cartaDobleSolapada)
+	cartaDobleSolapada = canvas.Replace("xx", valores[0], cartaDobleSolapada)
+	cartaDobleSolapada = canvas.Replace("PP", palos[0], cartaDobleSolapada)
 
-	valor, palo = cartas[1].Valor, cartas[1].Palo.String()
-	numStr = t.renderValorCarta(valor)
-	cartaDobleSolapada = canvas.Replace("yy", numStr, cartaDobleSolapada)
-	cartaDobleSolapada = canvas.Replace("QQ", palo[:2], cartaDobleSolapada)
+	cartaDobleSolapada = canvas.Replace("yy", valores[1], cartaDobleSolapada)
+	cartaDobleSolapada = canvas.Replace("QQ", palos[1], cartaDobleSolapada)
 
 	return cartaDobleSolapada
 }
 
-func (t templates) cartaTripleVisible(cartas []Carta) string {
+func (t templates) cartaDobleVisible(cartas []Carta) string {
+
+	valor1, palo1 := cartas[0].Valor, cartas[0].Palo.String()
+	numStr1 := t.renderValorCarta(valor1)
+
+	valor2, palo2 := cartas[1].Valor, cartas[1].Palo.String()
+	numStr2 := t.renderValorCarta(valor2)
+
+	valores := []string{numStr1, numStr2}
+	palos := []string{palo1[:2], palo2[:2]}
+
+	return mkCartaDobleVisible(valores, palos)
+}
+
+func (t templates) cartaDobleOculta(cartas []Carta) string {
+	valores := []string{"──", "──"}
+	palos := []string{"  ", "  "}
+	return mkCartaDobleVisible(valores, palos)
+}
+
+func mkCartaTripleVisible(valores, palos []string) string {
 	cartaDobleSolapada := canvas.Raw(`
 ┌xx┐yy┐zz┐
 │PP│QQ│RR│
 └──┘──┘──┘
 `)
+	cartaDobleSolapada = canvas.Replace("xx", valores[0], cartaDobleSolapada)
+	cartaDobleSolapada = canvas.Replace("PP", palos[0], cartaDobleSolapada)
 
-	valor, palo := cartas[0].Valor, cartas[0].Palo.String()
-	numStr := t.renderValorCarta(valor)
-	cartaDobleSolapada = canvas.Replace("xx", numStr, cartaDobleSolapada)
-	cartaDobleSolapada = canvas.Replace("PP", palo[:2], cartaDobleSolapada)
+	cartaDobleSolapada = canvas.Replace("yy", valores[1], cartaDobleSolapada)
+	cartaDobleSolapada = canvas.Replace("QQ", palos[1], cartaDobleSolapada)
 
-	valor, palo = cartas[1].Valor, cartas[1].Palo.String()
-	numStr = t.renderValorCarta(valor)
-	cartaDobleSolapada = canvas.Replace("yy", numStr, cartaDobleSolapada)
-	cartaDobleSolapada = canvas.Replace("QQ", palo[:2], cartaDobleSolapada)
-
-	valor, palo = cartas[2].Valor, cartas[2].Palo.String()
-	numStr = t.renderValorCarta(valor)
-	cartaDobleSolapada = canvas.Replace("zz", numStr, cartaDobleSolapada)
-	cartaDobleSolapada = canvas.Replace("RR", palo[:2], cartaDobleSolapada)
+	cartaDobleSolapada = canvas.Replace("zz", valores[2], cartaDobleSolapada)
+	cartaDobleSolapada = canvas.Replace("RR", palos[2], cartaDobleSolapada)
 
 	return cartaDobleSolapada
+}
+
+func (t templates) cartaTripleVisible(cartas []Carta) string {
+
+	valor1, palo1 := cartas[0].Valor, cartas[0].Palo.String()
+	numStr1 := t.renderValorCarta(valor1)
+
+	valor2, palo2 := cartas[1].Valor, cartas[1].Palo.String()
+	numStr2 := t.renderValorCarta(valor2)
+
+	valor3, palo3 := cartas[2].Valor, cartas[2].Palo.String()
+	numStr3 := t.renderValorCarta(valor3)
+
+	valores := []string{numStr1, numStr2, numStr3}
+	palos := []string{palo1[:2], palo2[:2], palo3[:2]}
+
+	return mkCartaTripleVisible(valores, palos)
+}
+
+func (t templates) cartaTripleOculta(cartas []Carta) string {
+
+	valores := []string{"──", "──", "──"}
+	palos := []string{"  ", "  ", "  "}
+
+	return mkCartaTripleVisible(valores, palos)
 }
