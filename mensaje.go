213f5365c96e3f420feb1912504b6017710a18f6
Juampi Filevich <juan.filevich@fing.edu.uy>
2020-08-05 09:51:08 -0300

    agregado tabla de mensajes

--- mensaje.go
+++ mensaje.go
@@ -5,6 +5,38 @@ import (
 	"encoding/json"
 )
 
+/*
+ * -----------------------------------
+ * | Cod. | Tipos de mensaje         |
+ * |------|--------------------------|
+ * |  00  | Error					 |
+ * |  01  | Nueva-Partida			 |
+ * |  02  | Nueva-Ronda				 |
+ * |  03  | Tirar-Carta				 |
+ * |  04  | Sumar-Puntos			 |
+ * |  05  | Fin-Partida				 |
+ * |  06  | Info					 |
+ * |=================================|
+ * |  07  | Toca-Envido				 |
+ * |  08  | Toca-RealEnvido			 |
+ * |  09  | Toca-FaltaEnvido		 |
+ * |---------------------------------|
+ * |  10  | Canta-Flor				 |
+ * |  11  | Canta-ContraFlor		 |
+ * |  12  | Canta-ContraFlorAlResto	 |
+ * |  13  | Responde-ConFlorMeAchico |
+ * |---------------------------------|
+ * |  14  | Responde-Quiero			 |
+ * |  15  | Responde-NoQuiero		 |
+ * |---------------------------------|
+ * |  16  | Mazo					 |
+ * |---------------------------------|
+ * |  17  | Grita-Truco				 |
+ * |  18  | Grita-ReTruco			 |
+ * |  19  | Grita-Vale4				 |
+ * -----------------------------------
+ */
+
 // Pkt ..
 type Pkt struct {
 	Dest []string // eso no debe tener JSON porque no lo va a usar
