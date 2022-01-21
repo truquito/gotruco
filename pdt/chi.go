44a800e60487371c1172736ea0ece958798939be
J.P. Filevich <juan.filevich@fing.edu.uy>
2022-01-21 06:38:20 -0300

    +Chi

--- pdt/chi.go
+++ pdt/chi.go
@@ -29,6 +29,48 @@ Respuestas
 // 3 tiradas + 12 jugadas = 15 acciones
 type A [15]bool // por default arranca en `false` todos
 
+func (A A) ToJugada(p *Partida, mix, aix int) IJugada {
+	esCarta := aix < 3
+
+	m := &p.Ronda.Manojos[mix]
+
+	if esCarta {
+		return TirarCarta{Manojo: m, Carta: *m.Cartas[aix]}
+	}
+
+	var jugada IJugada
+
+	switch aix {
+	case 3: // envido
+		jugada = TocarEnvido{Manojo: m}
+	case 4: // real-envido
+		jugada = TocarRealEnvido{Manojo: m}
+	case 5: // falta-envido
+		jugada = TocarFaltaEnvido{Manojo: m}
+	case 6: // flor
+		jugada = CantarFlor{Manojo: m}
+	case 7: // contra-flor
+		jugada = CantarContraFlor{Manojo: m}
+	case 8: // contra-flor-al-resto
+		jugada = CantarContraFlorAlResto{Manojo: m}
+	case 9: // truco
+		jugada = GritarTruco{Manojo: m}
+	case 10: // re-truco
+		jugada = GritarReTruco{Manojo: m}
+	case 11: // vale-4
+		jugada = GritarVale4{Manojo: m}
+	case 12: // quiero
+		jugada = ResponderQuiero{Manojo: m}
+	case 13: // no-Quiero
+		jugada = ResponderNoQuiero{Manojo: m}
+	case 14: // mazo
+		jugada = IrseAlMazo{Manojo: m}
+
+	}
+
+	return jugada
+}
+
 func (A A) String() string {
 	s := ""
 
@@ -123,4 +165,54 @@ func GetAA(p *Partida) []A {
 		res[i] = GetA(p, &p.Ronda.Manojos[i])
 	}
 	return res
+}
+
+// Retorna todas las acciones posibles para un jugador `m` dado
+func Chi(p *Partida, m *Manojo) []IJugada {
+
+	chi := make([]IJugada, 0, 15)
+
+	// tirada de cartas
+	for _, c := range m.Cartas {
+		j := TirarCarta{Manojo: m, Carta: *c}
+		_, ok := j.Ok(p)
+		if ok {
+			chi = append(chi, j)
+		}
+	}
+
+	// ijugada debe tener metodo ToCod
+	js := []IJugada{
+		// TirarCarta{},
+
+		// envite
+		TocarEnvido{Manojo: m},
+		TocarRealEnvido{Manojo: m},
+		TocarFaltaEnvido{Manojo: m},
+		CantarFlor{Manojo: m},
+		CantarContraFlor{Manojo: m},
+		CantarContraFlorAlResto{Manojo: m},
+		// { CantarConFlorMeAchico{Manojo: m}, enco.new },
+
+		// truco
+		GritarTruco{Manojo: m},
+		GritarReTruco{Manojo: m},
+		GritarVale4{Manojo: m},
+
+		// respuestas
+		ResponderQuiero{Manojo: m},
+		ResponderNoQuiero{Manojo: m},
+
+		// mazo
+		IrseAlMazo{Manojo: m},
+	}
+
+	for _, j := range js {
+		_, ok := j.Ok(p)
+		if ok {
+			chi = append(chi, j)
+		}
+	}
+
+	return chi
 }
