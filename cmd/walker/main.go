bec625d1f9576e8aaee9327189b4d6afb8be84ec
J.P. Filevich <juan.filevich@fing.edu.uy>
2024-01-09 20:56:11 -0300

    chore: update bench

--- cmd/walker/main.go
+++ cmd/walker/main.go
@@ -9,7 +9,6 @@ import (
 var terminals uint = 0
 
 func rec_play(p *pdt.Partida) {
-
 	bs, _ := p.MarshalJSON()
 
 	// para la partida dada, todas las jugadas posibles
@@ -30,15 +29,20 @@ func rec_play(p *pdt.Partida) {
 }
 
 func main() {
-
-	partidaJSON := `{"puntuacion":20,"puntajes":{"azul":19,"rojo":19},"ronda":{"manoEnJuego":0,"cantJugadoresEnJuego":{"azul":1,"rojo":1},"elMano":0,"turno":0,"envite":{"estado":"noCantadoAun","puntaje":0,"cantadoPor":"","sinCantar":null},"truco":{"cantadoPor":"","estado":"noGritadoAun"},"manojos":[{"seFueAlMazo":false,"cartas":[{"palo":"copa","valor":6},{"palo":"oro","valor":3},{"palo":"copa","valor":2}],"tiradas":[false,false,false],"ultimaTirada":0,"jugador":{"id":"Alvaro","equipo":"azul"}},{"seFueAlMazo":false,"cartas":[{"palo":"copa","valor":3},{"palo":"oro","valor":5},{"palo":"espada","valor":2}],"tiradas":[false,false,false],"ultimaTirada":0,"jugador":{"id":"Roro","equipo":"rojo"}}],"muestra":{"palo":"copa","valor":1},"manos":[{"resultado":"ganoRojo","ganador":"","cartasTiradas": []},{"resultado":"ganoRojo","ganador":"","cartasTiradas": []},{"resultado":"ganoRojo","ganador":"","cartasTiradas": []}]}}`
+	partidaJSON := `{"limiteEnvido":4,"cantJugadores":2,"puntuacion":20,"puntajes":{"azul":19,"rojo":19},"ronda":{"manoEnJuego":0,"cantJugadoresEnJuego":{"azul":1,"rojo":1},"elMano":0,"turno":0,"pies":[0,0],"envite":{"estado":"noCantadoAun","puntaje":0,"cantadoPor":""},"truco":{"cantadoPor":"","estado":"noGritadoAun"},"manojos":[{"seFueAlMazo":false,"cartas":[{"palo":"copa","valor":6},{"palo":"oro","valor":3},{"palo":"copa","valor":2}],"tiradas":[false,false,false],"ultimaTirada":0,"jugador":{"id":"Alvaro","nombre":"Alvaro","equipo":"azul"}},{"seFueAlMazo":false,"cartas":[{"palo":"copa","valor":3},{"palo":"oro","valor":5},{"palo":"espada","valor":2}],"tiradas":[false,false,false],"ultimaTirada":0,"jugador":{"id":"Roro","nombre":"Roro","equipo":"rojo"}}],"muestra":{"palo":"copa","valor":1},"manos":[{"resultado":"indeterminado","ganador":"","cartasTiradas":[]},{"resultado":"indeterminado","ganador":"","cartasTiradas":[]},{"resultado":"indeterminado","ganador":"","cartasTiradas":[]}]}}`
 	p, err := pdt.Parse(partidaJSON, true)
 	if err != nil {
 		panic(err)
 	}
-
 	rec_play(p)
-
 	fmt.Println("termino", terminals)
-
 }
+
+/*
+
+version: v0.2.x
+cpu: i5-12600k
+termino 1,807,482
+TIME:71.19 RAM:52352
+
+*/
